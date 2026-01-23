f217156db035edd2bd887412f41803a9cff65035
cristobyte <cristobal.delano@gmail.com>
2026-01-23 19:44:41 -0300

    allowed all hosts in vite config for sharing temporary public url

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true,
+    allowedHosts: ['.trycloudflare.com'],
   },
   optimizeDeps: {
     include: ['@hvac/shared'],
